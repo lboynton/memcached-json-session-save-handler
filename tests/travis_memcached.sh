59af1fc3fee2ebf489aa3abe25848e8b3c8379bc
Lee Boynton <lee@lboynton.com>
2013-02-24 17:25:40 +0000

    Attempt to install libmemcached

--- tests/travis_memcached.sh
+++ tests/travis_memcached.sh
@@ -1,2 +1,7 @@
-pecl install memcached
+sudo apt-get install libevent-dev libcloog-ppl0
+wget https://launchpad.net/libmemcached/1.0/1.0.13/+download/libmemcached-1.0.13.tar.gz
+tar xzf libmemcached-1.0.13.tar.gz
+cd libmemcached-1.0.13
+./configure && make && sudo make install
+printf "\n" | pecl install memcached
 echo "extension=memcached.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
