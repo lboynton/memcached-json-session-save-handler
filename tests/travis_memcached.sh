2dffde853cbc26cbcd29a9deb3f87fc0c4054aaa
Lee Boynton <lee@lboynton.com>
2013-02-24 17:29:58 +0000

    Try to fix memcached already loaded error in travis

--- tests/travis_memcached.sh
+++ tests/travis_memcached.sh
@@ -4,4 +4,3 @@ tar xzf libmemcached-1.0.13.tar.gz
 cd libmemcached-1.0.13
 ./configure && make && sudo make install
 printf "\n" | pecl install memcached
-echo "extension=memcached.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
