317bdd2d34e41431d6042649b59bb4a7efc4c0b8
Lee Boynton <lee@lboynton.com>
2013-02-24 17:35:56 +0000

    Try libmemcached 1.0.16

--- tests/travis_memcached.sh
+++ tests/travis_memcached.sh
@@ -1,6 +1,6 @@
 sudo apt-get install libevent-dev libcloog-ppl0
-wget https://launchpad.net/libmemcached/1.0/1.0.13/+download/libmemcached-1.0.13.tar.gz
-tar xzf libmemcached-1.0.13.tar.gz
-cd libmemcached-1.0.13
+wget https://launchpad.net/libmemcached/1.0/1.0.16/+download/libmemcached-1.0.16.tar.gz
+tar xzf libmemcached-1.0.16.tar.gz
+cd libmemcached-1.0.16
 ./configure && make && sudo make install
 printf "\n" | pecl install memcached
